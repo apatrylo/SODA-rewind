0415594897f4f09166b117a57a87058b8386ee2b
apatrylo <148206154+apatrylo@users.noreply.github.com>
2025-01-13 20:45:23 +0000

    Added flowbite "darkMode" class inside the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,68 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // Adjust this to match your folder structure
+    "./src/**/*.{html,js}", // Adjust to match your folder structure
     "./dist/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
+
+  darkMode: 'class', // Enable dark mode with the 'dark' class
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "50": "#f9fafb",
+          "100": "#f3f4f6",
+          "200": "#e5e7eb",
+          "300": "#d1d5db",
+          "400": "#9ca3af",
+          "500": "#6b7280",
+          "600": "#4b5563",
+          "700": "#374151",
+          "800": "#1f2937",
+          "900": "#111827",
+          "950": "#030712"
+        }
+      },
+      fontFamily: {
+        sans: [
+          'Lato',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ],
+        body: [
+          'Lato',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji'
+        ]
+      }
+    }
   },
+
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin') // Added Flowbite plugin
   ],
 };
