91355be657ca70251c55b9fc265369c18d63dac5
apatrylo <148206154+apatrylo@users.noreply.github.com>
2025-01-16 16:51:51 +0000

    Slider

--- dist/custom.js
+++ dist/custom.js
@@ -2,24 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const progressCircle = document.querySelector(".autoplay-progress svg circle");
     const progressContent = document.querySelector(".autoplay-progress span");
   
-    var swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
-        delay: 2500,
+        delay: 5000,
         disableOnInteraction: false,
       },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        dynamicBullets: true,
       },
       on: {
-        autoplayTimeLeft(s, time, progress) {
-          progressCircle.style.setProperty("--progress", 1 - progress);
+        autoplayTimeLeft(swiper, time, progress) {
+          const circumference = 125.6; // Circle's circumference
+          progressCircle.style.strokeDashoffset = circumference * (1 - progress);
           progressContent.textContent = `${Math.ceil(time / 1000)}s`;
         },
       },
